c72248218edd3697cf5b063980ba58470aa64be3
Hao <jusfeel@163.com>
2016-10-08 00:14:07 +0800

    comply to ember RESTSerializer format

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,56 @@
   var express = require('express');
   var router = express.Router();
   var mongojs = require('mongojs');
+	var modelName = 'entry';
 	var collName = 'entries';
+	var nameSpace = 'api/v1';
   var db = mongojs('npcp', [collName]);
  
   router.get('/', function(req, res) {
     res.render('index');
   });
  
-  router.get('/api/' + collName, function(req, res) {
+  router.get('/' + nameSpace + '/' + collName, function(req, res) {
     db[collName].find(function(err, data) {
-      res.json(data);
+			var _obj = {};
+			_obj[modelName] = data;
+      res.json(_obj);
     });
   });
  
-  router.post('/api/' + collName, function(req, res) {
-    db[collName].insert(req.body, function(err, data) {
-      res.json(data);
+  router.post('/' + nameSpace + '/' + collName, function(req, res) {
+		var _obj = req.body[modelName];
+    db[collName].insert(_obj, function(err, data) {
+			var _obj = {};
+			_obj[modelName] = data;
+      res.json(_obj);
     });
  
   });
  
-  router.put('/api/' + collName, function(req, res) {
-		var _id = req.body._id;
-    var _obj = req.body; 
+  router.patch('/' + nameSpace + '/' + collName, function(req, res) {
+
+		var _id = req.body[modelName]._id;
+    var _obj = req.body[modelName]; 
 		delete _obj._id;
 
     db[collName].update({
       _id: mongojs.ObjectId(_id)
     }, _obj, {}, function(err, data) {
-      res.json(data);
+			var _obj = {};
+			_obj[modelName] = data;
+      res.json(_obj);
     });
  
   });
  
-  router.delete('/api/' + collName + '/:_id', function(req, res) {
+  router.delete('/' + nameSpace + '/' + collName + '/:_id', function(req, res) {
     db[collName].remove({
       _id: mongojs.ObjectId(req.params._id)
     }, '', function(err, data) {
-      res.json(data);
+			var _obj = {};
+			_obj[modelName] = data;
+      res.json(_obj);
     });
  
   });
